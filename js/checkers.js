f8c66de93d709597c833af0b76e9c412e8a712f8
JoshuaChan308 <55455749+JoshuaChan308@users.noreply.github.com>
2020-02-18 13:17:50 -0500

    /

--- js/checkers.js
+++ js/checkers.js
@@ -1,14 +1,15 @@
 let board;
 let turn;
 let win;
-let x_wins_count = 0
-let o_wins_count = 0
-let switch_turn_count = 0
+let x_wins = 0
+let o_wins = 0
+let determine_first_player;
+
 const message = document.querySelector("h2");
 document.getElementById("board").onclick = takeTurn;
 
 window.onload = init()
-
+ 
 function init() {
   board = ["", "", "", "", "", "", "", "",
            "", "", "", "", "", "", "", "",
